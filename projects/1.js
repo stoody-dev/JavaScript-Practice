df26f8f71d6aea5df987d6604efb6a2844b75b59
AKSHAY <forakshay.dev@gmail.com>
2023-02-22 20:32:19 +0530

    Update 1.js

---
+++ projects/1.js
@@ -0,0 +1,6 @@
+// operator , 2 numbers , result 
+// input , result output
+// if else 
+// result display console.log
+
+const operator = prompt('Enter Operator (either +,-,*,/):')   ;                        // prompt  for input 
